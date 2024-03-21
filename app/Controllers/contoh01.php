06b29b22c4cf39da6c51fee02806eed20ae45362
michorsptra <michorespati343@gmail.com>
2024-03-21 13:29:14 +0700

    tugas 01

---
+++ app/Controllers/contoh01.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controllers;
+
+class contoh01 extends BaseController
+{
+    public function index()
+    {
+        echo "<h1>Perkenalan diri</h1>";
+        echo "Nama saya Micho Respati Putra\n
+            Hobi saya Mendengarkan musik";
+    }
+}
